a7a629ba86c2962b1988026f58cded31c683bf18
joaquim <joaquim01001@gmail.com>
2025-08-12 16:43:49 -0300

    inclusao de novas features

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const input = document.getElementById("amigo");
 const buttonAdd = document.querySelector(".button-add")
 const friendsList = document.getElementById("listaAmigos");
 const buttonDraw = document.querySelector(".button-draw");
+const randomFriendList = document.getElementById("resultado");
 
 const render = () => {
     const name = input.value;
@@ -13,6 +14,7 @@ const render = () => {
     if (isValidInput) {
         friends.push(name);
         updateFriendsList();
+        input.focus();
     } else {
         alert("Por favor, insira um nome válido.");
     }
@@ -34,7 +36,7 @@ const renderRandomFriend = () => {
     if (friends.length > 0) {
         const indexRandomFriend = random(friends.length);
         let randomFriend = document.getElementById(`friend-${indexRandomFriend}`);
-        randomFriend.style.color = "purple";
+        randomFriendList.innerHTML = `<li><span>${randomFriend.textContent}</span></li>`;
     } else {
         alert("Você deve adicionar pelo menos um amigo.")
     }
@@ -43,5 +45,10 @@ const renderRandomFriend = () => {
 
 const random = size => Math.floor(Math.random() * friends.length);
 
+input.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        render();
+    }
+})
 buttonAdd.addEventListener("click", render);
 buttonDraw.addEventListener("click", renderRandomFriend);
